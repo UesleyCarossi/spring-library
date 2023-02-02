caf3f1e9f13105f14a0638719f0a0a04b5c66999
uesley.carossi1 <uesley.carossi@evoluum.com.br>
2023-02-02 09:01:08 -0300

    first commit

---
+++ src/test/java/com/uesley/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uesley.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
